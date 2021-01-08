52eb245e9567d50bbd5d1f9a08d87dcf5eda0571
emadas <73523988+emadas@users.noreply.github.com>
2021-01-08 20:57:27 +0200

    Add files via upload

---
+++ signup-check.php
@@ -0,0 +1,92 @@
+<?php 
+session_start(); 
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])
+    && isset($_POST['name']) && isset($_POST['re_password'])
+    &&isset($_POST['email'])&&isset($_POST['phone'])
+    &&isset($_POST['uaddress'])){
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+	$ad = validate($_POST['uaddress']);
+	$em = validate($_POST['email']);
+	$ph = validate($_POST['phone']);
+	$re_pass = validate($_POST['re_password']);
+	$name = validate($_POST['name']);
+
+	$user_data = 'uname='. $uname. '&name='. $name;
+
+
+	if (empty($uname)) {
+		header("Location: signup.php?error=User Name is required&$user_data");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: signup.php?error=Password is required&$user_data");
+	    exit();
+	}
+	else if(empty($re_pass)){
+        header("Location: signup.php?error=Re Password is required&$user_data");
+	    exit();
+	}
+
+	else if(empty($name)){
+        header("Location: signup.php?error=Name is required&$user_data");
+	    exit();
+	}
+
+
+	else if(empty($em)){
+        header("Location: signup.php?error=email is required&$user_data");
+	    exit();
+	}else if(empty($ph)){
+        header("Location: signup.php?error=phone is required&$user_data");
+	    exit();
+	}else if(empty($ad)){
+        header("Location: signup.php?error=address is required&$user_data");
+	    exit();
+	}
+
+
+
+	else if($pass !== $re_pass){
+        header("Location: signup.php?error=The confirmation password  does not match&$user_data");
+	    exit();
+	}
+
+	else{
+
+		// hashing the password
+        $pass = md5($pass);
+
+	    $sql = "SELECT * FROM users WHERE user_name='$uname' ";
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) > 0) {
+			header("Location: signup.php?error=The username is taken try another&$user_data");
+	        exit();
+		}else {
+           $sql2 = "INSERT INTO users(name, user_name, password, email, phone, uaddress) 
+           VALUES('$name', '$uname', '$pass','$em', '$ph', '$ad')";
+           $result2 = mysqli_query($conn, $sql2);
+           if ($result2) {
+           	 header("Location: login.php?success=Your account has been created successfully");
+	         exit();
+           }else {
+	           	header("Location: signup.php?error=unknown error occurred&$user_data");
+		        exit();
+           }
+		}
+	}
+	
+}
+else{
+	header("Location: signup.php");
+	exit();
+}
