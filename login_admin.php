3f25ace36ac1c4a106e8ec7a93941f9ccbdf13c6
ahmadzrekat <73077472+ahmadzrekat@users.noreply.github.com>
2021-01-10 11:00:06 +0200

    Add files via upload

---
+++ login_admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title>LOGIN</title>
+     <link rel="stylesheet" type="text/css" href="stylelogin.css">
+</head>
+<body>
+     <form action="db_login_admin.php" method="post">
+          <h2>LOGIN ADMIN</h2>
+          <?php if (isset($_GET['error'])) { ?>
+               <p class="error"><?php echo $_GET['error']; ?></p>
+          <?php } ?>
+          <label>User Name</label>
+          <input type="text" name="uname" placeholder="User Name"><br>
+
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Password"><br>
+
+          <button type="submit">Login</button>
+         
+     </form>
+</body>
+</html>
+
